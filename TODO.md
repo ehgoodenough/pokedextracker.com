567cbc0447a168adc9ba58c79b65c36f9763cf3b
Andrew McPherson <mcphandr@amazon.com>
2019-06-02 13:58:25 -0700

    Finished the streamer config options.
    - Matched the style of the form to pokedextracker's styles.
    - Added a listener to the form to call the Twitch Config service.
    - Set the default values of the form from the Redux store.

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
-let the streamer set config values.
-manual reload button
+when a viewer "opens in new tab, open it on pokedextracker.com"
+link to just open it in pokedextracker.com?
+viewer manual reload button
 how to bundle and upload?
--- app/components/twitch.js
+++ app/components/twitch.js
@@ -1,29 +1,65 @@
 import React from "react"
+import * as ReactRedux from "react-redux"
+import TwitchExt from "twitchext"
 
-export class TwitchConfigComponent extends React.Component {
+class TwitchConfig extends React.Component {
     render() {
+        // if(this.props.twitch.isReady != true) {
+        //     return (
+        //         <div className="loading">
+        //             Loading...
+        //         </div>
+        //     )
+        // }
+        this.props.twitch.configuration = this.props.twitch.configuration || {}
         return (
             <div className="twitch-config">
-                Hello World!
-                <input type="text" id="username" name="username" onSubmit={this.onSubmit}/>
-                <input type="text" id="dexslug" name="dexslug" onSubmit={this.onSubmit}/>
+                <section className="options">
+                    <form onSubmit={this.onSubmit}>
+                        <div className="option">
+                            <label htmlFor="username">
+                                <span className="name">Username</span>
+                                <span className="url">/u/<b>ashketchum10</b></span>
+                            </label>
+                            <input type="text" id="username" name="username" defaultValue={this.props.twitch.configuration.username} placeholder="ashketchum10"/>
+                        </div>
+                        <div className="option">
+                            <label htmlFor="dexslug">
+                                <span className="name">Dex</span>
+                                <span className="url">/u/ashketchum10/<b>living-dex</b></span>
+                            </label>
+                            <input type="text" id="dexslug" name="dexslug" defaultValue={this.props.twitch.configuration.dexslug} placeholder="living-dex"/>
+                        </div>
+                        <div className="option">
+                            <input className="submit" type="submit" value="Save"/>
+                            {this.confirmation}
+                        </div>
+                    </form>
+                </section>
+                <section className="instructions">
+                </section>
             </div>
         )
+    }
+    get confirmation() {
+        if(this.state && this.state.confirmationDate) {
+            return (
+                <div className="confirmation" key={this.state.confirmationDate}>
+                    Saved to your extension!
+                </div>
+            )
+        }
     }
     get onSubmit() {
         return (event) => {
             event.preventDefault()
-            console.log({
-                "username": document.getElementById("username").value,
-                "dexslug": document.getElementById("dexslug").value,
-            })
-            // TwitchExt.configuration.set("broadcaster", "0.0.1", JSON.stringify({
-            //     "username": document.getElementById("username").value,
-            //     "dexslug": document.getElementById("dexslug").value,
-            // }))
+            const username = document.getElementById("username").value
+            const dexslug = document.getElementById("dexslug").value
+            TwitchExt.configuration.set("broadcaster", "0.0.1", JSON.stringify({ username, dexslug }))
+            this.setState({"confirmationDate": Date.now()})
+            // TODO: Also push the data to the local store.
         }
     }
 }
 
-// TODO: Read the initial values from the store! Redux.connect this.
-// TODO: Style the config page to look halfway decent, with instructions on where to see the extension.
+export const TwitchConfigComponent = ReactRedux.connect(({twitch}) => ({twitch}))(TwitchConfig)
